bc4e420c3bc4f1f1512ea2bb56991080489f1509
Nikk6174 <nikk.6174@gmail.com>
2025-07-09 12:21:25 +0530

    Enhance model training and evaluation process
    - Updated `time_left.tsv` with new iteration data.
    - Added hyperparameter tuning for various models in `model_trainer.py` using GridSearchCV.
    - Modified `evaluate_model` function in `utils.py` to accept hyperparameters and implement grid search for model optimization.

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -52,7 +52,48 @@ class ModelTrainer:
                 "AdaBoost Classifier": AdaBoostRegressor()
             }
 
-            model_report:dict = evaluate_model(X_train = X_train, y_train = y_train,X_test= X_test,y_test= y_test , models = models)
+            params = {
+                "Linear Regression": {},
+                "Lasso Regression": {
+                    'alpha': [0.1, 0.5, 1.0, 1.5, 2.0],
+                    'max_iter': [1000, 2000, 3000]
+                },
+                "Ridge Regression": {
+                    'alpha': [0.1, 0.5, 1.0, 1.5, 2.0],
+                    'max_iter': [1000, 2000, 3000]
+                },
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "XGBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "K-Neighbors Classifier": {
+                    'n_neighbors': [3, 5, 7, 9],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "CatBoosting Classifier": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Classifier": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
+
+            model_report:dict = evaluate_model(X_train = X_train, y_train = y_train,X_test= X_test,y_test= y_test , models = models, param = params)
 
             ##to get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
