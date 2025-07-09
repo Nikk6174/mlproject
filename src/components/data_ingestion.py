2eec28334aea639c91842408e9ba65e204941655
Nikk6174 <nikk.6174@gmail.com>
2025-07-09 12:20:41 +0530

    Add model training functionality and evaluation metrics
    - Introduced `ModelTrainer` class to handle model training and selection.
    - Implemented `evaluate_model` function to assess multiple regression models using R² score.
    - Integrated model training into the data ingestion pipeline.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+
 from dataclasses import dataclass
 
 @dataclass
@@ -56,5 +59,8 @@ if __name__ == "__main__":
     train_data, test_data, _ = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
             
